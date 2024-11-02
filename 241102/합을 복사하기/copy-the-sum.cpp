0b1cd2e943a63aa3bfb9affcac7203857bcd0f4a
CHIPCHIP <79151948+yisu0128@users.noreply.github.com>
2024-11-02 18:52:36 +0900

    Title: 합을 복사하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241102/합을 복사하기/copy-the-sum.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    int a = 1;
+    int b = 2;
+    int c = 3;
+    int s = a+b+c;
+    a=b=c=s;
+    cout << a<< " "<<b<<" "<<c;
+    return 0;
+}
