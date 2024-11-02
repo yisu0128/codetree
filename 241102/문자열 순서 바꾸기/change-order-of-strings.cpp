1bccc59156cd55fc1a3e3b687e7437da53e67929
CHIPCHIP <79151948+yisu0128@users.noreply.github.com>
2024-11-02 22:57:09 +0900

    Title: 문자열 순서 바꾸기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241102/문자열 순서 바꾸기/change-order-of-strings.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    string s,t;
+    cin >> s>> t;
+    cout << t << endl << s;
+    return 0;
+}
