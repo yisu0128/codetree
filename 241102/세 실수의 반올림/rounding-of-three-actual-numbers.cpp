62e9b6ba6d2bd3e84ad14cf1de858a62871e0946
CHIPCHIP <79151948+yisu0128@users.noreply.github.com>
2024-11-02 22:32:57 +0900

    Title: 세 실수의 반올림, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241102/세 실수의 반올림/rounding-of-three-actual-numbers.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cout << fixed;
+    cout.precision(3);
+    double a, b, c;
+    cin >> a;
+    cin >> b;
+    cin >> c;
+    cout << a << endl << b<<endl<<c;
+    return 0;
+}
