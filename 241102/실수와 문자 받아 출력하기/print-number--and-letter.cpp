0779ca4c1cd0a58c84421c32d411a959cfb154cc
CHIPCHIP <79151948+yisu0128@users.noreply.github.com>
2024-11-02 22:54:15 +0900

    Title: 실수와 문자 받아 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241102/실수와 문자 받아 출력하기/print-number--and-letter.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cout << fixed;
+    cout.precision(2);
+    char c;
+    double a,b;
+    cin >> c >> a >> b;
+    cout << c <<endl <<a << endl << b;
+    return 0;
+}
