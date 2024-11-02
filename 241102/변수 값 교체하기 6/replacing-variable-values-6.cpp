586682d950fff60c9c388ca6692e93c385473619
CHIPCHIP <79151948+yisu0128@users.noreply.github.com>
2024-11-02 18:40:19 +0900

    Title: 변수 값 교체하기 6, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241102/변수 값 교체하기 6/replacing-variable-values-6.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a =2, b=5;
+    int temp;
+    temp = a;
+    a = b;
+    b=temp;
+
+    cout <<a<<endl<<b;
+    return 0;
+}
