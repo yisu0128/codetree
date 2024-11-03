10599c2956fd616437d6263d8e4db55909f456bc
CHIPCHIP <79151948+yisu0128@users.noreply.github.com>
2024-11-04 00:05:45 +0900

    Title: 전화번호 바꾸기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241104/전화번호 바꾸기/change-phone-number.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int yyyy;
+    int xxxx;
+    char dash;
+    string o;
+
+    cin >> o >> dash >>  yyyy >> dash >> xxxx;
+    cout << o << dash << xxxx << dash << yyyy;
+
+    
+    return 0;
+}
