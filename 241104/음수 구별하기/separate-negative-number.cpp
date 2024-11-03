456ba596f09aa78ffd28d0aa9bb5ecd6c84810d4
CHIPCHIP <79151948+yisu0128@users.noreply.github.com>
2024-11-04 08:41:21 +0900

    Title: 음수 구별하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241104/음수 구별하기/separate-negative-number.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+    cout << n;
+    if (n < 0){
+        cout << endl << "minus";
+    }
+    return 0;
+}
