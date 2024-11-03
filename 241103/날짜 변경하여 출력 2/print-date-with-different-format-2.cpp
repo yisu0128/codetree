7f7d4206344dbc52108fb4cb5cd5881527a94d42
CHIPCHIP <79151948+yisu0128@users.noreply.github.com>
2024-11-03 12:43:48 +0900

    Title: 날짜 변경하여 출력 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241103/날짜 변경하여 출력 2/print-date-with-different-format-2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int y;
+    int m;
+    int d;
+    char dash;
+
+    cin >> m >> dash >> d >> dash >> y;
+    cout << y << "." << m << "." << d;
+    return 0;
+}
