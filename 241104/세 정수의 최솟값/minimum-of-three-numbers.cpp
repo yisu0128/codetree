8f6fb70024824170d787f0a1f3ba57b1ef8d5c44
CHIPCHIP <79151948+yisu0128@users.noreply.github.com>
2024-11-04 20:00:16 +0900

    Title: 세 정수의 최솟값, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241104/세 정수의 최솟값/minimum-of-three-numbers.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a,b,c;
+    cin>> a>>b>>c;
+    if(a<=b && a<=c){
+        cout << a;
+    }
+    else if (b <a && b<=c){
+        cout << b;
+    }
+    else{
+        cout <<c;
+    }
+    return 0;
+}
