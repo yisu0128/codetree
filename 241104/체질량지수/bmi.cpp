7ae00cb86c36c08cc8a7715116ba93a845f445ee
CHIPCHIP <79151948+yisu0128@users.noreply.github.com>
2024-11-04 19:28:31 +0900

    Title: 체질량지수, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

--- 241104/체질량지수/bmi.cpp
+++ 241104/체질량지수/bmi.cpp
@@ -4,13 +4,14 @@ using namespace std;
 int main() {
     // 여기에 코드를 작성해주세요.
     int h, w, b;
-    b= (10000*w)/(h^2);
-
     cin >> h>>w;
+    b= (10000*w)/(h*h);
+
+    
 
     cout << b;
     if (b>=25) {
-        cout <<endl<< "obesity";
+        cout <<endl<< "Obesity";
     }
     return 0;
 }
