ea3b1f42eadb74a054ffeeb157021ade2bbf1e5f
CHIPCHIP <79151948+yisu0128@users.noreply.github.com>
2024-11-04 19:30:42 +0900

    Title: 정사각형의 넓이, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241104/정사각형의 넓이/area-of-a-rectangle.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+    cout << n*n;
+    if (n<5){
+        cout << endl << "tiny";
+    }
+    return 0;
+}
