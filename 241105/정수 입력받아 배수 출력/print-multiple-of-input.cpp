47449b7ad3eb41100898a752f28ec79cd34e18d9
CHIPCHIP <79151948+yisu0128@users.noreply.github.com>
2024-11-05 08:22:55 +0900

    Title: 정수 입력받아 배수 출력, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 241105/정수 입력받아 배수 출력/print-multiple-of-input.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n, i;
+    cin>> n;
+    for (i=n; i<=5*n; i+=n;){
+       cout << i << " ";
+    }
+    return 0;
+}
