71ae5bf97861f8f896d55435c8bbd8d4f7ad3ef3
CHIPCHIP <79151948+yisu0128@users.noreply.github.com>
2024-11-05 18:37:23 +0900

    Title: 별 n개 출력하기, Time: 1088ms, Memory: 23MB, Status: Time Limit Exceed - CodeTree

---
+++ 241105/별 n개 출력하기/print-n-stars.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int star, i;
+    cin >> star;
+    while (i<= star){
+        cout << i << " ";
+    }
+    return 0;
+}
