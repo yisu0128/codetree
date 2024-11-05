3d2862a6d910b7e00e2bc00eff3e2ae82b891936
CHIPCHIP <79151948+yisu0128@users.noreply.github.com>
2024-11-05 23:38:03 +0900

    Title: a/b 출력, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241105/a/b 출력/a-divide-b.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a,b, i;
+    cin>> a>> b;
+    cout << (int)a/b<<".";
+    for (i=1; i<=20; i++){
+        cout << (int)(10*a)/b;
+        a = (10*a)%b;
+    }
+    
+    return 0;
+}
