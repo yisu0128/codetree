037f7fbfbecc856254e6f3db805459c488634474
CHIPCHIP <79151948+yisu0128@users.noreply.github.com>
2024-11-05 23:53:11 +0900

    Title: n번 더하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241105/n번 더하기/add-n-times.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a,b;
+    cin>> a>>b;
+    for (int i=0; i<b; i++){
+        a+=b;
+        cout << a <<endl;
+    }
+    return 0;
+}
