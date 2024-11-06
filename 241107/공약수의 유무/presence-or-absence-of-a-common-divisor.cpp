2b748d9451fea34546d24372acf14aabbb98a13e
CHIPCHIP <79151948+yisu0128@users.noreply.github.com>
2024-11-07 00:05:43 +0900

    Title: 공약수의 유무, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241107/공약수의 유무/presence-or-absence-of-a-common-divisor.cpp
+++ 241107/공약수의 유무/presence-or-absence-of-a-common-divisor.cpp
@@ -6,8 +6,8 @@ int main() {
     int a,b; 
     int yn = 0;
     cin >> a >>b;
-    for (int i=2; i<=a; i++){
-        if(a%i == 0 && b%i==0){
+    for (int i=a; i<=b; i++){
+        if(1920%i == 0 && 2880%i==0){
             yn = 1;
             break;
         }
