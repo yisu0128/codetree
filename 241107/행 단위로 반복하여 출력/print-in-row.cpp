734f5b1c55ba3c471d5af4a84c23362b1c5108c3
CHIPCHIP <79151948+yisu0128@users.noreply.github.com>
2024-11-07 23:48:32 +0900

    Title: 행 단위로 반복하여 출력, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241107/행 단위로 반복하여 출력/print-in-row.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+    for (int i =0; i<3; i++){
+        for (int j =1; j<=3; j++){
+            cout<< j;
+        }
+        cout << endl;
+    }
+    return 0;
+}
