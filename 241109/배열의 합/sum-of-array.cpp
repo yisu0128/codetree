c2b43071cd2ca7e4bd4169a8dd63b4d6695f4d00
CHIPCHIP <79151948+yisu0128@users.noreply.github.com>
2024-11-09 12:32:44 +0900

    Title: 배열의 합, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241109/배열의 합/sum-of-array.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int arr[4][4];
+    
+    for (int i =0; i<4; i++){
+        int sum_val = 0;
+        for (int j = 0; j<4; j++){
+            cin >> arr[i][j];
+            sum_val += arr[i][j];
+        }
+        cout << sum_val <<endl;
+
+    }
+    return 0;
+}
