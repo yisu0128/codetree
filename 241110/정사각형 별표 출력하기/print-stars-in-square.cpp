f5017642c74c9433f754a26437989d077061b5af
CHIPCHIP <79151948+yisu0128@users.noreply.github.com>
2024-11-10 17:10:47 +0900

    Title: 정사각형 별표 출력하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241110/정사각형 별표 출력하기/print-stars-in-square.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n; 
+    cin >> n;
+    for (int i=0; i<4; i++){
+        for (int j=0; j<4; j++){
+            cout << "*";
+
+        }
+        cout << endl;
+    }
+    return 0;
+}
